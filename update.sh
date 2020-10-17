e68c1d569ee38e95025f0aaed97d0d1eb02f53a9
Hetsh <git@hetsh.de>
2020-10-17 18:20:15 +0200

    Use update_url function

--- update.sh
+++ update.sh
@@ -21,21 +21,7 @@ IMG_CHANNEL="stable"
 update_image "library/debian" "Debian" "false" "$IMG_CHANNEL-\d+-slim"
 
 # Terraria Server
-CURRENT_APP_VERSION="${_CURRENT_VERSION%-*}"
-MIRROR="https://terraria.org"
-VERSION_REGEX="\d{4}"
-DOWNLOAD_URI=$(curl --silent --location "$MIRROR" | grep -P -o "(?<=href=.).*$VERSION_REGEX\.zip")
-NEW_APP_VERSION=$(echo $DOWNLOAD_URI | grep -P -o "$VERSION_REGEX(?=.zip)" | sed 's|.|&\.|g' | sed 's|\.$||')
-if [ "$CURRENT_APP_VERSION" != "$NEW_APP_VERSION" ]; then
-	prepare_update "" "Terraria Server" "$CURRENT_APP_VERSION" "$NEW_APP_VERSION"
-	update_version "$NEW_APP_VERSION"
-
-	# Since the terraria server is not a regular package, the version number needs
-	# to be replaced with the url to download the archive
-	_UPDATES[-3]="APP_URL"
-	_UPDATES[-2]="\".*\""
-	_UPDATES[-1]="\"${MIRROR}${DOWNLOAD_URI}\""
-fi
+update_url "APP_URL" "Terraria Server" "true" "https://terraria.org" "(?<=href=(\"|')).*/terraria-server-\d{4}\.zip" "\d{4}(?=.zip)"
 
 # Packages
 PKG_URL="https://packages.debian.org/$IMG_CHANNEL/amd64"
